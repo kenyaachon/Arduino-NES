e9eb6d8c6aad5503a6c8bdc98eabc88ac8fd605f
mosesmbugua <swahili1963@gmail.com>
2023-01-04 22:08:05 -0800

    Initialy Setup

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include "SD_MMC.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+    if(!SD_MMC.begin()){
+        Serial.println("Card Mount Failed");
+        return;
+    }
+    uint8_t cardType = SD_MMC.cardType();
+
+    if(cardType == CARD_NONE){
+        Serial.println("No SD_MMC card attached");
+        return;
+    }
+
+    Serial.print("SD_MMC Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
