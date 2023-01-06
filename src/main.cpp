6c54ebf0dae06dfae85ace2c30eb1b8236925438
mosesmbugua <swahili1963@gmail.com>
2023-01-05 22:16:30 -0800

    Different experiments to get the SD card reader to work

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,63 @@
 #include <Arduino.h>
-#include "SD_MMC.h"
+// #include "SD_MMC.h"
+#include "SPI.h"
+#include "SD.h"
+
+#define SD_CS 5
+
+SPIClass spi = SPIClass(VSPI);
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(SD_CS, OUTPUT);
   Serial.begin(115200);
-    if(!SD_MMC.begin()){
-        Serial.println("Card Mount Failed");
-        return;
-    }
-    uint8_t cardType = SD_MMC.cardType();
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 
-    if(cardType == CARD_NONE){
-        Serial.println("No SD_MMC card attached");
-        return;
-    }
+  spi.begin(SCK, MISO, MOSI, SS);
 
-    Serial.print("SD_MMC Card Type: ");
-    if(cardType == CARD_MMC){
-        Serial.println("MMC");
-    } else if(cardType == CARD_SD){
-        Serial.println("SDSC");
-    } else if(cardType == CARD_SDHC){
-        Serial.println("SDHC");
-    } else {
-        Serial.println("UNKNOWN");
-    }
+  Serial.print("MOSI: ");
+  Serial.println(MOSI);
+  Serial.print("MISO: ");
+  Serial.println(MISO);
+  Serial.print("SCK: ");
+  Serial.println(SCK);
+  Serial.print("SS: ");
+  Serial.println(SS);
+// if(!SD_MMC.begin(SS, spi, 80000000)){
+//         Serial.println("Card Mount Failed");
+//         return;
+//     }
+//     uint8_t cardType = SD_MMC.cardType();
+
+//     if(cardType == CARD_NONE){
+//         Serial.println("No SD_MMC card attached");
+//         return;
+//     }
+
+//     Serial.print("SD_MMC Card Type: ");
+//     if(cardType == CARD_MMC){
+//         Serial.println("MMC");
+//     } else if(cardType == CARD_SD){
+//         Serial.println("SDSC");
+//     } else if(cardType == CARD_SDHC){
+//         Serial.println("SDHC");
+//     } else {
+//         Serial.println("UNKNOWN");
+//     }
+
+
+if(!SD.begin(SS, spi, 80000000)){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
 }
 
 void loop() {
