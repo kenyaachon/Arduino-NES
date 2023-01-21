eef32dad7be26b34c4e932cfabfd59dce627c267
mosesmbugua <swahili1963@gmail.com>
2023-01-20 22:18:47 -0800

    Working version of the SD card

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,13 @@
 #include "SPI.h"
 #include "SD.h"
 
-#define SD_CS 5
+SPIClass spi = SPIClass(SPI);
 
-SPIClass spi = SPIClass(VSPI);
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  pinMode(SD_CS, OUTPUT);
-  Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
 
-  spi.begin(SCK, MISO, MOSI, SS);
+  Serial.begin(115200);
 
   Serial.print("MOSI: ");
   Serial.println(MOSI);
@@ -24,42 +18,38 @@ void setup() {
   Serial.print("SCK: ");
   Serial.println(SCK);
   Serial.print("SS: ");
-  Serial.println(SS);
-// if(!SD_MMC.begin(SS, spi, 80000000)){
-//         Serial.println("Card Mount Failed");
-//         return;
-//     }
-//     uint8_t cardType = SD_MMC.cardType();
+  Serial.println(SS); 
 
-//     if(cardType == CARD_NONE){
-//         Serial.println("No SD_MMC card attached");
-//         return;
-//     }
+  spi.begin(SCK, MISO, MOSI, SS);
 
-//     Serial.print("SD_MMC Card Type: ");
-//     if(cardType == CARD_MMC){
-//         Serial.println("MMC");
-//     } else if(cardType == CARD_SD){
-//         Serial.println("SDSC");
-//     } else if(cardType == CARD_SDHC){
-//         Serial.println("SDHC");
-//     } else {
-//         Serial.println("UNKNOWN");
-//     }
-
-
-if(!SD.begin(SS, spi, 80000000)){
+  if (!SD.begin(SS,spi,80000000)) {
     Serial.println("Card Mount Failed");
     return;
   }
+
   uint8_t cardType = SD.cardType();
 
   if(cardType == CARD_NONE){
     Serial.println("No SD card attached");
     return;
   }
+
+  Serial.print("SD Card Type: ");
+  if(cardType == CARD_MMC){
+    Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+    Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // Serial.println("Hello world");
 }
